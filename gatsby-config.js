a6ef49a7e5731ded236c332fda53f614dc3ebd0d
Vikram Jadhav <vikramcse.10@gmail.com>
2018-12-15 23:05:17 +0530

    added google analytics tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,7 +49,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `UA-130227707-1`,
+        trackingId: `UA-131081058-1`,
       },
     },
     `gatsby-plugin-feed`,
